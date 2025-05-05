56f4dba6205ad6ae76e236683c2665d78558cab8
Your Name <you@example.com>
2025-05-04 21:17:24 -0400

    new try

--- treemap.c
+++ treemap.c
@@ -193,21 +193,7 @@ Recuerde hacer que el current apunte al nodo encontrado.
 
 */
 
-Pair * searchTreeMap(TreeMap * tree, void* key) {
-    if (tree == NULL || tree->root == NULL) return NULL;
-    TreeNode* node = tree->root;
-    while (node != NULL) {
-        if (!tree->lower_than(key, node->pair->key) && !tree->lower_than(node->pair->key, key)) {
-            tree->current = node;
-            return node->pair;
-        } else if (tree->lower_than(key, node->pair->key)) {
-            node = node->left;
-        } else {
-            node = node->right;
-        }
-    }
-    return NULL;
-}
+
 
 
 
@@ -222,10 +208,37 @@ Para implementarla puede realizar una búsqueda normal y usar un puntero a nodo 
 
     }*/
 
-Pair * upperBound(TreeMap * tree, void* key) {
-    return NULL;
-}
+Pair* upperBound(TreeMap* tree, void* key) {
+    if (tree == NULL || tree->root == NULL) return NULL;
+    
 
+    Pair* found = searchTreeMap(tree, key);
+    if (found != NULL) return found;
+    
+        
+    TreeNode* node = tree->root;
+    TreeNode* ub_node = NULL;
+    
+    while (node != NULL) 
+    {
+        if (tree->lower_than(key, node->pair->key))
+         {
+            ub_node = node;
+                node = node->left;
+            } 
+        else {
+                node = node->right;
+            }
+        }
+    
+        if (ub_node != NULL) {
+            tree->current = ub_node;
+            return ub_node->pair;
+        }
+    
+        return NULL;
+    }
+    
 /*6.- Implemente las funciones para recorrer la estructura: Pair* firstTreeMap(TreeMap* tree) retorna el primer **Pair** del mapa 
 (el menor). Pair* nextTreeMap(TreeMap* tree)  retornar el siguiente **Pair**
  del mapa a partir del puntero TreeNode* current. Recuerde actualizar este puntero.
